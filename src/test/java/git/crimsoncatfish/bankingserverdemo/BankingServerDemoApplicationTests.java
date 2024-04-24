aae9b6a2c70fddf005f4dfd01f07c57c36e55dab
crimson-catfish <sava_kto@mail.ru>
2024-04-24 12:37:31 +0300

    init

---
+++ src/test/java/git/crimsoncatfish/bankingserverdemo/BankingServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package git.crimsoncatfish.bankingserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
