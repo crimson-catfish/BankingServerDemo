963b56ffbef5c4c0fa4f04fb9fc803f094bbd792
crimson-catfish <sava_kto@mail.ru>
2024-04-29 21:15:37 +0300

    feat: user template in database and in pojo

---
+++ src/main/java/ru/ivanov_savelii/bankingserverdemo/model/User.java
@@ -0,0 +1,24 @@
+package ru.ivanov_savelii.bankingserverdemo.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+public class User implements Serializable {
+
+    private Long id;
+    private String login;
+    private String passhash;
+    private BigDecimal balance;
+
+    public User() {}
+
+    public User(String login, String passhash, BigDecimal balance) {
+        this.login = login;
+        this.passhash = passhash;
+        this.balance = balance;
+    }
+}
