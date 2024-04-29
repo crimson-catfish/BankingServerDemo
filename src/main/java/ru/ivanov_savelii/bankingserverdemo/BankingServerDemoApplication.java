0b6cb0ec4304d8c3ffeb3b9d8de53c342cb46351
crimson-catfish <sava_kto@mail.ru>
2024-04-29 21:16:51 +0300

    feat: configured application and dependencies (I hope correctly this time)

--- src/main/java/ru/ivanov_savelii/bankingserverdemo/BankingServerDemoApplication.java
+++ src/main/java/ru/ivanov_savelii/bankingserverdemo/BankingServerDemoApplication.java
@@ -1,13 +1,11 @@
-package git.crimsoncatfish.bankingserverdemo;
+package ru.ivanov_savelii.bankingserverdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankingServerDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BankingServerDemoApplication.class, args);
     }
-
 }
