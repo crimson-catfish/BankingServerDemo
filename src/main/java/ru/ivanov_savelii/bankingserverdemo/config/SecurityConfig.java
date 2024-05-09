e009e6849dfe310fc1197aa369cbeae30350d56d
crimson-catfish <sava_kto@mail.ru>
2024-05-09 19:58:27 +0300

    feat: SecurityConfig for Jwt authorization system

---
+++ src/main/java/ru/ivanov_savelii/bankingserverdemo/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package ru.ivanov_savelii.bankingserverdemo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${secret-key}")
+    private String jwtSecretKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/signup").permitAll()
+                        .requestMatchers("/signin").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                ))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder () {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(jwtSecretKey.getBytes(), "");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+}
