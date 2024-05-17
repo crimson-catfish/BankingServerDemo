c39e3e0a88e047a0c7d75bc8b1a92c51e207e330
crimson-catfish <sava_kto@mail.ru>
2024-05-17 21:26:37 +0300

    fix: installation instructions

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,5 @@ create table if not exists users
     id       int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     login    text    not null,
     password text    not null,
-    balance  numeric not null,
-    PRIMARY KEY (ID)
+    balance  numeric not null
 );
